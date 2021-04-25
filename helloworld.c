b5263317d3c2af3ced04580dd0b157b9a803eefa
lythanhvien <vienthanhlyc9@gmail.com>
2021-04-25 18:32:01 +0700

    hello git

---
+++ helloworld.c
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "Enter 2 number";
+    cin >> a >> b;
+    cout << a+b;
+    return 0;
+    
+}
+
+
+// #include <stdio.h>
+// int main() {
+//    // printf() displays the string inside quotation
+//    printf("Hello, World!\n");
+//    return 0;
+// }
---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout << "Enter number 1: ";
+    cin >> a;
+    cout << "\nEnter number 2: ";
+    cin >> b;
+    cout << "Sum of 2 numbers is: ";
+    cout << a+b;
+    return 0;
+}
+
+
+// #include <stdio.h>
+// int main() {
+//    // printf() displays the string inside quotation
+//    printf("Hello, World!\n");
+//    return 0;
+// }
